cafdef595693a21dbe3cbb55d2adec93720e4cb8
Derrick <derrickkueh1995@gmail.com>
2025-03-06 17:02:51 +0800

    commit original images

---
+++ js/book.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#flipbook").turn({
+        width: 800,  // Set book width
+        height: 600, // Set book height
+        autoCenter: true, // Center book in viewport
+        acceleration: true, // Enable smooth flipping
+    });
+});
