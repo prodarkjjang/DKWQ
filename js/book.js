557269df9627bc82e10f93bef471765808e23628
Derrick <derrickkueh1995@gmail.com>
2025-03-07 17:00:50 +0800

    added new book2

--- js/book.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("#flipbook").turn({
-        width: 800,  // Set book width
-        height: 600, // Set book height
-        autoCenter: true, // Center book in viewport
-        acceleration: true, // Enable smooth flipping
-    });
-});
